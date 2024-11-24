7c1ee25d232cdc4f135388021d1ba99bbc4a6b99
Nico <nicompj@gmail.com>
2024-11-23 20:39:58 -0500

    Coburgos

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+    
+    menuButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
+});
+
